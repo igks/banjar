584d58e96d668bdfd11a10933aa0bff8474355b0
I Gusti Kade <sugiantara.igk@gmail.com>
2021-04-03 18:22:26 +0700

    draft dashboard

---
+++ resources/views/layouts/my-app.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+
+<body class="font-sans antialiased" style="font-family: Nunito;">
+  <div class="py-3 px-2">
+    <div class="mb-1">
+      <div id="menu" class="flex flex-row justify-between items-center mb-2" onclick="showMenu()">
+        <div class="flex flex-row">
+          <i data-feather="menu" id="open-menu" class=""></i>
+          <i data-feather="x-circle" id="close-menu" class="hidden"></i>
+          <p class="ml-2">Menu</p>
+        </div>
+        <div class="text-center">
+          <p class="font-bold">
+            Laporan
+          </p>
+          <p class="font-bold">
+            Banjar Batu Aji Barat
+          </p>
+        </div>
+      </div>
+
+      <div id="menu-list" class="p-3 rounded-md w-full hidden">
+        <ul>
+          <li class="border-b px-4 mb-2 ">
+            <a href="{{url('/')}}">
+              <div class="flex flex-row justify-between">
+                <p>Login</p>
+                <i data-feather="chevrons-right"></i></i>
+              </div>
+            </a>
+          </li>
+
+          <li class="border-b px-4 mb-2 ">
+            <a href="{{url('/')}}">
+              <div class="flex flex-row justify-between">
+                <p>Login</p>
+                <i data-feather="chevrons-right"></i></i>
+              </div>
+            </a>
+          </li>
+
+          <li class="border-b px-4 mb-2 ">
+            <a href="{{url('/')}}">
+              <div class="flex flex-row justify-between">
+                <p>Login</p>
+                <i data-feather="chevrons-right"></i></i>
+              </div>
+            </a>
+          </li>
+
+          <li class="border-b px-4 mb-2 ">
+            <a href="{{url('/')}}">
+              <div class="flex flex-row justify-between">
+                <p>Login</p>
+                <i data-feather="chevrons-right"></i></i>
+              </div>
+            </a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+    <hr>
+    @yield('content')
+  </div>
+
+  <script src="https://unpkg.com/feather-icons"></script>
+  <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <script>
+    feather.replace();
+      function showMenu(){
+            let menuList = document.getElementById('menu-list');
+            let openMenuIcon = document.getElementById('open-menu');
+            let closeMenuIcon = document.getElementById('close-menu');
+
+            menuList.classList.toggle('hidden');
+            openMenuIcon.classList.toggle('hidden');
+            closeMenuIcon.classList.toggle('hidden');
+            
+        }
+  </script>
+  @yield('script')
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,180 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.my-app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
-</head>
-
-<body class="antialiased">
-    <div>
-        <a href="{{url('login')}}" class="bg-">Login</a>
+@section('content')
+<div class="px-2">
+    <div id="content" class="w-full mt-3">
+        <div class="bg-blue-200 rounded-md p-2">
+            <div class="flex flex-row justify-between mb-2">
+                <p class="font-bold text-lg">Kas Banjar</p>
+            </div>
+            <hr>
+            <div class="flex flex-row justify-end mt-2">
+                <i data-feather="dollar-sign"></i>
+                <p class="font-bold text-2xl">1000000</p>
+            </div>
+        </div>
     </div>
-</body>
 
-</html>
+    <div id="content" class="w-full mt-3">
+        <div class="bg-green-200 rounded-md p-2">
+            <div class="flex flex-row justify-between mb-2">
+                <p class="font-bold text-lg">Tabungan Nyepi</p>
+            </div>
+            <hr>
+            <div class="flex flex-row justify-end mt-2">
+                <i data-feather="dollar-sign"></i>
+                <p class="font-bold text-2xl">1000000</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="content" class="w-full mt-3">
+        <div class="bg-yellow-200 rounded-md p-2">
+            <div class="flex flex-row justify-between mb-2">
+                <p class="font-bold text-lg">Tabungan Piodalan</p>
+            </div>
+            <hr>
+            <div class="flex flex-row justify-end mt-2">
+                <i data-feather="dollar-sign"></i>
+                <p class="font-bold text-2xl">1000000</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="content" class="w-full mt-3">
+        <div class="bg-red-200 rounded-md p-2">
+            <div class="flex flex-row justify-between mb-2">
+                <p class="font-bold text-lg">Kas Banten</p>
+            </div>
+            <hr>
+            <div class="flex flex-row justify-end mt-2">
+                <i data-feather="dollar-sign"></i>
+                <p class="font-bold text-2xl">1000000</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="content" class="w-full mt-3">
+        <div class="bg-gray-100 rounded-md p-2">
+            <div class="flex flex-row justify-between mb-2">
+                <p class="font-bold text-lg">Informasi</p>
+            </div>
+            <hr>
+            <div class="my-2">
+                <p class="text-center text-lg font-bold">Banjar Bau Aji Barat terdiri dari 29 KK (26 Keluarga dan 3 Muda
+                    Mudi)</p>
+            </div>
+            <hr>
+            <div class="my-2">
+                <p class="text-lg font-bold">Rincian Iuran Banjar</p>
+                <div class="mt-2 mb-5">
+                    <p>A. Iuran perbulan Banjar Batu Aji Barat adalah <strong>100.000</strong>, dibagi
+                        menjadi:</p>
+                    <table class="w-full">
+                        <tr>
+                            <td>Kas Banjar</td>
+                            <td>13.000</td>
+                        </tr>
+                        <tr>
+                            <td>Iuran BOP</td>
+                            <td>40.000</td>
+                        </tr>
+                        <tr>
+                            <td>Iuran Tabungan Nyepi</td>
+                            <td>15.000</td>
+                        </tr>
+                        <tr>
+                            <td>Iuran Tabungan Piodalan</td>
+                            <td>20.000</td>
+                        </tr>
+                        <tr>
+                            <td>Iuran Banten</td>
+                            <td>8.000</td>
+                        </tr>
+                        <tr>
+                            <td>Iuran WHDI</td>
+                            <td>2.000</td>
+                        </tr>
+                        <tr>
+                            <td>Operasional Pengurus</td>
+                            <td>2.000</td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class="mt-2 mb-5">
+                    <p>B. Iuran perbulan Muda Mudi adalah <strong>75.000</strong>, dibagi menjadi:</p>
+                    <table class="w-full">
+                        <tr>
+                            <td>Iuran BOP</td>
+                            <td>40.000</td>
+                        </tr>
+                        <tr>
+                            <td>Iuran Tabungan Nyepi</td>
+                            <td>15.000</td>
+                        </tr>
+                        <tr>
+                            <td>Iuran Tabungan Piodalan</td>
+                            <td>20.000</td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class="mt-2 mb-5">
+                    <p>C. Arisan perbulan <strong>50.000</strong></p>
+                </div>
+
+                <div class="mt-2 mb-5">
+                    <p>D. Iuran Pembangunan Pura <strong>1000.000</strong>/KK dan dicicil 10 kali.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="content" class="w-full mt-3">
+        <div class="rounded-md p-2">
+            <div class="flex flex-row justify-between mb-2">
+                <p class="font-bold text-lg">Riwayat Pengurus</p>
+            </div>
+            <hr>
+            <table class="my-2">
+                <th>Periode 2021 - 2022</th>
+                <tr>
+                    <td>Ketua</td>
+                    <td>: Nyoman Arsanayasa </td>
+                </tr>
+                <tr>
+                    <td>Bendahara</td>
+                    <td>: Ni Wayan Yuli Astuti </td>
+                </tr>
+                <tr>
+                    <td>Bendahara Banten</td>
+                    <td>: Rini Megasari</td>
+                </tr>
+            </table>
+            <hr>
+            <table class="my-2">
+                <th>Periode 2020 - 2021</th>
+                <tr>
+                    <td>Ketua</td>
+                    <td>: Made Suardana </td>
+                </tr>
+                <tr>
+                    <td>Bendahara</td>
+                    <td>: Ni Wayan Yuli Astuti </td>
+                </tr>
+                <tr>
+                    <td>Bendahara Banten</td>
+                    <td>: Sri Soemarni</td>
+                </tr>
+            </table>
+            <hr>
+        </div>
+    </div>
+
+</div>
+@endsection
+
+@section('script')
+
+@endsection
