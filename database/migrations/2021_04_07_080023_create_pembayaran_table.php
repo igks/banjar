2b5ee7484b091c828e8e296c790a5a4f48ec9925
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2021-04-07 15:20:13 +0700

    create table pembayaran and transaction

---
+++ database/migrations/2021_04_07_080023_create_pembayaran_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembayaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_master_id')->reference('id')->on('member_master');
+            $table->string('tahun', 4);
+            $table->tinyInteger('bulan');
+            $table->decimal('jumlah', 10, 2);
+            $table->text('keterangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+}
